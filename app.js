b761ed81f006cf38a4e738292b284d4c3b5da808
johndev07 <101162480+johndev07@users.noreply.github.com>
2024-02-28 12:44:56 +0530

    added cors package to access from other domain

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const connectToDatabase = require("./db/dbConfig");
 const questionRoutes = require("./routes/questionRoutes");
+var cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
+
 connectToDatabase();
 // Use routes
 app.use("/questions", questionRoutes);
